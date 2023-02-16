50789de74ba3fa85bd0286093b9b41155bf13049
Kostas9999 <b00148740@mytudublin.ie>
2023-02-16 10:55:46 +0000

    Update compareBaseline.js

--- baseLine/compareBaseline.js
+++ baseLine/compareBaseline.js
@@ -1,196 +1,199 @@
 const { client } = require("../database/connections/db_pg_connection");
 let { getBaselineBuff, updateBaselineBuff } = require("./getBaseline");
 const onEvent = require("../events/onEvent");
-let buff_passive = {};
 
-async function passive(data) {
-  let buff_passive = {};
-  if (Object.keys(buff_passive).length == 0) {
-    await client.query(`SET search_path TO '${data.UID}';`);
-    let row = await client.query(`SELECT relese, build FROM os LIMIT 1`);
+try {
+  async function passive(data) {
+    let buff_passive = {};
+    if (Object.keys(buff_passive).length == 0) {
+      await client.query(`SET search_path TO '${data.UID}';`);
+      let row = await client.query(`SELECT relese, build FROM os LIMIT 1`);
 
-    buff_passive = {
-      relese: row.rows[0].relese.trim(),
-      build: row.rows[0].build.trim(),
-    };
-  }
+      buff_passive = {
+        relese: row.rows[0].relese.trim(),
+        build: row.rows[0].build.trim(),
+      };
+    }
 
-  if (buff_passive.relese != data.data.os.relese) {
-    onEvent.onEvent({
-      type: "OS_VER",
-      data: {
-        UID: data.UID,
-        reading: data.data.os.relese,
-        baseline: buff_passive.relese,
-      },
-    });
-    buff_passive = {};
-  }
-
-  if (buff_passive.build != data.data.os.build) {
-    onEvent.onEvent({
-      type: "OS_VER",
-      data: {
-        UID: data.UID,
-        reading: data.data.os.build,
-        baseline: buff_passive.build,
-      },
-    });
-    buff_passive = {};
-  }
-
-  getBaselineBuff(data.UID).then((baseline) => {
-    if (data.data.hardware.TotalMemory != baseline.memorytotal) {
+    if (buff_passive.relese != data.data.os.relese) {
       onEvent.onEvent({
-        type: "MEM_TOT",
+        type: "OS_VER",
         data: {
           UID: data.UID,
-          reading: data.data.hardware.TotalMemory,
-          baseline: baseline.memorytotal,
+          reading: data.data.os.relese,
+          baseline: buff_passive.relese,
         },
       });
+      buff_passive = {};
     }
-  });
-}
-async function mid(data) {
-  data.data.disc.forEach((disc) => {
-    if (disc.use > 85) {
+
+    if (buff_passive.build != data.data.os.build) {
       onEvent.onEvent({
-        type: "DSC_FULL",
+        type: "OS_VER",
         data: {
           UID: data.UID,
-          reading: { disc: disc.fs, use: disc.use },
-          baseline: 85,
+          reading: data.data.os.build,
+          baseline: buff_passive.build,
         },
       });
+      buff_passive = {};
     }
-  });
 
-  data.data.ports.forEach((port) => {
     getBaselineBuff(data.UID).then((baseline) => {
-      if (!("," + baseline.ports + ",").includes("," + port.port + ",")) {
+      if (data.data.hardware.TotalMemory != baseline.memorytotal) {
         onEvent.onEvent({
-          type: "PRT_NEW",
+          type: "MEM_TOT",
           data: {
             UID: data.UID,
-            reading: port.port,
-            baseline: baseline.ports,
+            reading: data.data.hardware.TotalMemory,
+            baseline: baseline.memorytotal,
           },
         });
       }
     });
-  });
-
-  data.data.arp.forEach((arp) => {
-    getBaselineBuff(data.UID).then((baseline) => {
-      if (!baseline.neighbours.includes(arp.mac)) {
+  }
+  async function mid(data) {
+    data.data.disc.forEach((disc) => {
+      if (disc.use > 85) {
         onEvent.onEvent({
-          type: "NGH_NEW",
+          type: "DSC_FULL",
           data: {
             UID: data.UID,
-            reading: { ip: arp.ip, mac: arp.mac },
-            baseline: baseline.neighbours,
+            reading: { disc: disc.fs, use: disc.use },
+            baseline: 85,
           },
         });
       }
     });
-  });
-}
 
-async function active(data) {
-  getBaselineBuff(data.UID).then((baseline) => {
-    if (data.data.memory > baseline.memoryuses_t) {
+    data.data.ports.forEach((port) => {
+      getBaselineBuff(data.UID).then((baseline) => {
+        if (!("," + baseline.ports + ",").includes("," + port.port + ",")) {
+          onEvent.onEvent({
+            type: "PRT_NEW",
+            data: {
+              UID: data.UID,
+              reading: port.port,
+              baseline: baseline.ports,
+            },
+          });
+        }
+      });
+    });
+
+    data.data.arp.forEach((arp) => {
+      getBaselineBuff(data.UID).then((baseline) => {
+        if (!baseline.neighbours.includes(arp.mac)) {
+          onEvent.onEvent({
+            type: "NGH_NEW",
+            data: {
+              UID: data.UID,
+              reading: { ip: arp.ip, mac: arp.mac },
+              baseline: baseline.neighbours,
+            },
+          });
+        }
+      });
+    });
+  }
+
+  async function active(data) {
+    getBaselineBuff(data.UID).then((baseline) => {
+      if (data.data.memory > baseline.memoryuses_t) {
+        onEvent.onEvent({
+          type: "MEM_USE",
+          data: {
+            UID: data.UID,
+            reading: data.data.memory,
+            baseline: baseline.memoryuses_t,
+          },
+        });
+      }
+
+      if (!baseline.defaultgateway.includes(data.data.networkStats.dgMAC)) {
+        onEvent.onEvent({
+          type: "GTW_ADR",
+          data: {
+            UID: data.UID,
+            reading: data.data.networkStats.dgMAC,
+            baseline: baseline.defaultgateway,
+          },
+        });
+      }
+
+      let n = baseline.locallatency * (baseline.locallatency_t / 100);
+      if (data.data.networkStats.localLatency > n) {
+        onEvent.onEvent({
+          type: "LAT_LOC",
+          data: {
+            UID: data.UID,
+            reading: data.data.networkStats.localLatency,
+            baseline: n,
+          },
+        });
+      }
+
+      let m = baseline.publiclatency * (baseline.publiclatency_t / 100);
+      if (data.data.networkStats.publicLatency > m) {
+        onEvent.onEvent({
+          type: "LAT_PUB",
+          data: {
+            UID: data.UID,
+            reading: data.data.networkStats.publicLatency,
+            baseline: m,
+          },
+        });
+      }
+    });
+
+    if (data.data.networkStats.rx_dropped) {
       onEvent.onEvent({
-        type: "MEM_USE",
+        type: "RX_DRP",
         data: {
           UID: data.UID,
-          reading: data.data.memory,
-          baseline: baseline.memoryuses_t,
+          reading: data.data.networkStats.rx_dropped,
+          baseline: 0,
         },
       });
     }
 
-    if (!baseline.defaultgateway.includes(data.data.networkStats.dgMAC)) {
+    if (data.data.networkStats.rx_error) {
       onEvent.onEvent({
-        type: "GTW_ADR",
+        type: "RX_ERR",
         data: {
           UID: data.UID,
-          reading: data.data.networkStats.dgMAC,
-          baseline: baseline.defaultgateway,
+          reading: data.data.networkStats.rx_error,
+          baseline: 0,
         },
       });
     }
 
-    let n = baseline.locallatency * (baseline.locallatency_t / 100);
-    if (data.data.networkStats.localLatency > n) {
+    if (data.data.networkStats.tx_dropped) {
       onEvent.onEvent({
-        type: "LAT_LOC",
+        type: "TX_DRP",
         data: {
           UID: data.UID,
-          reading: data.data.networkStats.localLatency,
-          baseline: n,
+          reading: data.data.networkStats.tx_dropped,
+          baseline: 0,
         },
       });
     }
 
-    let m = baseline.publiclatency * (baseline.publiclatency_t / 100);
-    if (data.data.networkStats.publicLatency > m) {
+    if (data.data.networkStats.tx_error) {
       onEvent.onEvent({
-        type: "LAT_PUB",
+        type: "TX_ERR",
         data: {
           UID: data.UID,
-          reading: data.data.networkStats.publicLatency,
-          baseline: m,
+          reading: data.data.networkStats.tx_error,
+          baseline: 0,
         },
       });
     }
-  });
 
-  if (data.data.networkStats.rx_dropped) {
-    onEvent.onEvent({
-      type: "RX_DRP",
-      data: {
-        UID: data.UID,
-        reading: data.data.networkStats.rx_dropped,
-        baseline: 0,
-      },
-    });
+    // console.log(data.data.networkStats.rx_dropped);
   }
-
-  if (data.data.networkStats.rx_error) {
-    onEvent.onEvent({
-      type: "RX_ERR",
-      data: {
-        UID: data.UID,
-        reading: data.data.networkStats.rx_error,
-        baseline: 0,
-      },
-    });
-  }
-
-  if (data.data.networkStats.tx_dropped) {
-    onEvent.onEvent({
-      type: "TX_DRP",
-      data: {
-        UID: data.UID,
-        reading: data.data.networkStats.tx_dropped,
-        baseline: 0,
-      },
-    });
-  }
-
-  if (data.data.networkStats.tx_error) {
-    onEvent.onEvent({
-      type: "TX_ERR",
-      data: {
-        UID: data.UID,
-        reading: data.data.networkStats.tx_error,
-        baseline: 0,
-      },
-    });
-  }
-
-  // console.log(data.data.networkStats.rx_dropped);
+} catch (error) {
+  console.log(error);
 }
 /*
 
