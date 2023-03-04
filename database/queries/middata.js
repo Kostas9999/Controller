298b3800d232aba361ef174b8d18be72e8cee5d5
Kostas9999 <91334044+Kostas9999@users.noreply.github.com>
2023-03-04 23:29:14 +0000

    Update middata.js

--- database/queries/middata.js
+++ database/queries/middata.js
@@ -29,9 +29,11 @@ module.exports = async function (data) {
 
         try {
           client.query(`SET search_path TO '${data.UID}';`);
+        
 
           client.query(
-            `INSERT INTO "${data.UID}"."ports" ( ${ports_keys}  ) VALUES (${values_String})  ON CONFLICT (port) DO UPDATE SET (${ports_keys}, created) = ( ${values_String}, now();`
+            
+            `INSERT INTO "${data.UID}"."ports" ( ${ports_keys}  ) VALUES (${values_String})  ON CONFLICT (port) DO UPDATE SET (${ports_keys}, created) = ( ${values_String}, now());`
           );
         } catch (error) {
           console.log(error);
